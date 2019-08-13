dd16c74e7ec6c5064df9260828bf082ad9c9660c
David Jones <drj@pobox.com>
2019-08-13 10:53:20 +0100

    Dummy version of Python script for gatepython-plugin

---
+++ hyperpartisan_gate.py
@@ -0,0 +1,37 @@
+# hyperpartisan_gate.py
+# This (Python) script is intended to be launched via
+# gateplugin-python https://github.com/GateNLP/gateplugin-python
+
+import os
+import sys
+
+noisy = False
+if os.environ.get("USER") == "ac1xdrj":
+    noisy = True
+
+if noisy: print("hyper starting", file=sys.stderr, flush=True)
+
+if noisy:
+    with open(__file__) as me:
+        sys.stderr.write(me.read())
+
+from gate import executable
+
+if noisy: print("imported gate", file=sys.stderr)
+
+print("Python version", sys.version, file=sys.stderr, flush=True)
+
+# This version adds a dummy annotation,
+# it doesn't do any useful work yet.
+# But does demonstrate the interoperation.
+
+@executable
+def hyper(document, outputAS):
+    document.annotationSets[outputAS].add(
+        0, len(document.text), "hyperpartisan",
+        dict(confidence=0.91, hyperpartisan_label="maybe")
+    )
+
+
+if __name__ == "__main__":
+    hyper.start()
