18a8a9add6d592016a8a92a2b48828ecef3867b3
David Jones <drj@pobox.com>
2019-08-14 09:39:37 +0100

    Split into functions

--- hyperpartisan.py
+++ hyperpartisan.py
@@ -20,27 +20,56 @@ from Preprocessing import line2elmo2
 
 
 def hyperpartisan(text):
-    pipeline = xml2line.create_pipeline()
 
     article = {
         "id": "command line text",
         "xml": text,
-        "et": xml.etree.ElementTree.fromstring(
-            '<article title="command line text" />')
     }
+
+    add_article_sent(article)
+
+    json.dump(article, sys.stdout, indent=2)
+
+    vectors = elmo_embedding(article)
+
+    json.dump([v.tolist() for v in vectors], sys.stdout, indent=2)
+    print()
+
+
+def add_article_sent(article):
+    """
+    To the article represented by `article`, add
+    a "article_sent" key which is the tokenised text
+    of the article joined with spaces into a single string.
+    (and perform other processing necessary to get to this point)
+    """
+
+    article["et"] = xml.etree.ElementTree.fromstring(
+            '<article title="command line text" />')
+
+    pipeline = xml2line.create_pipeline()
     nerror = utils.run_pipeline(pipeline, article)
     article["nerror"] = nerror
 
     del article["et"]
 
-    json.dump(article, sys.stdout, indent=2)
+    return article
+
+
+def elmo_embedding(article):
+    """
+    Convert an article (using the text in its "article_sent" key)
+    to embedded ELMo vectors.
+
+    A 2-dimensional array is returned,
+    with one row per sentence.
+    """
 
     sents = article["article_sent"].split(" <splt> ")
 
     elmo = line2elmo2.create_elmo("original", False)
     vectors = line2elmo2.elmo_one_article(elmo, sents, 200, 200, batchsize=50,)
-    json.dump([v.tolist() for v in vectors], sys.stdout,
-    indent=2)
+    return vectors
 
 
 def main(argv=None):
