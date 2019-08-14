6d2eb1918d350a800c8138d52c3e302f9e491f29
David Jones <drj@pobox.com>
2019-08-14 09:47:59 +0100

    Add main function

--- ensemble_pred.py
+++ ensemble_pred.py
@@ -56,38 +56,42 @@ def ensemble(models,model_input):
     return model
 
 
-parser = OptionParser()
-parser.add_option("--inputTSV", help="load saved cache", type=str)
-parser.add_option("--output", help="load saved cache", type=str)
-parser.add_option("--saved_model1", help="load saved cache", type=str)
-parser.add_option("--saved_model2", help="load saved cache", type=str)
-parser.add_option("--saved_model3", help="load saved cache", type=str)
+def main():
+    parser = OptionParser()
+    parser.add_option("--inputTSV", help="load saved cache", type=str)
+    parser.add_option("--output", help="load saved cache", type=str)
+    parser.add_option("--saved_model1", help="load saved cache", type=str)
+    parser.add_option("--saved_model2", help="load saved cache", type=str)
+    parser.add_option("--saved_model3", help="load saved cache", type=str)
 
-options, arguments = parser.parse_args()
+    options, arguments = parser.parse_args()
 
-max_len = 200
-embed_size = 1024
-seed = 7
+    max_len = 200
+    embed_size = 1024
+    seed = 7
 
-x_data, y_data, doc_id = load_data(options.inputTSV,max_len=max_len)
+    x_data, y_data, doc_id = load_data(options.inputTSV,max_len=max_len)
 
-model1 = load_model(options.saved_model1)
-model1.name = 'model1'
-model2 = load_model(options.saved_model2)
-model2.name = 'model2'
-model3 = load_model(options.saved_model3)
-model3.name = 'model3'
+    model1 = load_model(options.saved_model1)
+    model1.name = 'model1'
+    model2 = load_model(options.saved_model2)
+    model2.name = 'model2'
+    model3 = load_model(options.saved_model3)
+    model3.name = 'model3'
 
-models = [model1, model2, model3]
+    models = [model1, model2, model3]
 
-print(models[0].input_shape[1:])
-model_input = Input(shape=models[0].input_shape[1:], dtype='float32')
+    print(models[0].input_shape[1:])
+    model_input = Input(shape=models[0].input_shape[1:], dtype='float32')
 
-ensemble_models = ensemble(models,model_input)
+    ensemble_models = ensemble(models,model_input)
 
-pred = ensemble_models.predict(x_data)
+    pred = ensemble_models.predict(x_data)
 
-all_pred = toEvaluationFormat(doc_id, pred)
-with open(options.output, 'w') as fo:
-    for item in all_pred:
-        fo.write(item)
+    all_pred = toEvaluationFormat(doc_id, pred)
+    with open(options.output, 'w') as fo:
+        for item in all_pred:
+            fo.write(item)
+
+if __name__ == "__main__":
+    main()
